3eb6d8253b564d7f72a10405a0ed1427d7e91f7e
AnitaSteb <anita10012@gmail.com>
2024-05-13 13:19:40 +0300

    changes in c#

--- migdalor/ConsoleApp/Program.cs
+++ migdalor/ConsoleApp/Program.cs
@@ -1,10 +1,10 @@
-﻿//using ClassLibrary1.Models;
+﻿using ClassLibrary1.Models;
 
-//Console.WriteLine("Hello, World!");
-//MigdalorContext db = new MigdalorContext();
+Console.WriteLine("Hello, World!");
+MigdalorContext db = new MigdalorContext();
 
-//foreach (TblUser user in db.TblUsers)
-//{
-//    Console.WriteLine($"{user.UserId}, {user.Username}, " +
-//        $"{user.Password}, {user.RoleNumber}, {user.RoleName}");
-//}
+foreach (TblUser user in db.TblUsers)
+{
+    Console.WriteLine($"{user.UserId}, {user.Username}, " +
+        $"{user.Password}, {user.RoleNumber}, {user.RoleName}");
+}
