ab24e673e855937ea4990a655c5e8cf84adc1964
AnitaSteb <anita10012@gmail.com>
2024-05-20 12:31:24 +0300

    Login Controller

---
+++ Migdalor_Backend/WebApplication1/DTO/UserLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.DTO
+{
+    public class UserLogin
+    {
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
