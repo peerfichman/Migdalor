ae399a594bcb63e676ecdd98967e4789d450ee1c
AnitaSteb <anita10012@gmail.com>
2024-05-21 18:52:30 +0300

    Changes

--- Migdalor_Backend/WebApplication1/Program.cs
+++ Migdalor_Backend/WebApplication1/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (true)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
