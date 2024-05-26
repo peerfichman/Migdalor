b7e10fcd13406edec06fb8f660a1d1a01ed8af10
Aner95 <ozeraner18@gmail.com>
2024-05-26 13:12:29 +0300

    home3

--- migdalor/src/Components/AddTenant.jsx
+++ migdalor/src/Components/AddTenant.jsx
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
-import './styled123.css';
+import './AddTenantStyle.css';
+
+
+const generateRandomPassword = () => {
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const numbers = '0123456789';
+  let password = '';
+  for (let i = 0; i < 3; i++) {
+    password += letters.charAt(Math.floor(Math.random() * letters.length));
+  }
+  for (let i = 0; i < 3; i++) {
+    password += numbers.charAt(Math.floor(Math.random() * numbers.length));
+  }
+  return password;
+};
 
 const AddTenant = () => {
   const [formData, setFormData] = useState({
@@ -10,14 +24,18 @@ const AddTenant = () => {
     entryDate: '',
     previousCity: '',
     phone: '',
+    additionalPhone: '',
+    email: '',
+    additionalEmail: '',
     profession: '',
     relativePhone: '',
     relativeContact: '',
     picture: null,
-    hobbies: ''
+    selectedHobbies: []
   });
 
   const [successMessage, setSuccessMessage] = useState('');
+  const [userDetails, setUserDetails] = useState({ username: '', password: '' });
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -27,9 +45,21 @@ const AddTenant = () => {
     });
   };
 
+  const handleHobbyChange = (e) => {
+    const hobby = e.target.value;
+    setFormData((prevData) => {
+      const selectedHobbies = prevData.selectedHobbies.includes(hobby)
+        ? prevData.selectedHobbies.filter((h) => h !== hobby)
+        : [...prevData.selectedHobbies, hobby].slice(0, 10); // Limit to 10 hobbies
+      return { ...prevData, selectedHobbies };
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    const password = generateRandomPassword();
+    const username = formData.idNumber;
+    setUserDetails({ username, password });
     setSuccessMessage(`הדייר ${formData.firstName} ${formData.lastName} נוסף בהצלחה`);
   };
 
@@ -43,32 +73,51 @@ const AddTenant = () => {
       entryDate: '',
       previousCity: '',
       phone: '',
+      additionalPhone: '',
+      email: '',
+      additionalEmail: '',
       profession: '',
       relativePhone: '',
       relativeContact: '',
       picture: null,
-      hobbies: ''
+      selectedHobbies: []
     });
+    setUserDetails({ username: '', password: '' });
   };
+
+  const handleUserDetailsClose = () => {
+    setUserDetails({ username: '', password: '' });
+  };
+
+  const hobbiesList = [
+    "קריאה", "כתיבה", "ציור", "בישול", "אפייה", "טיפוח גינה", "ספורט", "ריצה", "שחייה",
+    "רכיבה על אופניים", "טיולים בטבע", "דיג", "נגרות", "סרגיה", "משחקי קופסה", "שחמט",
+    "פאזלים", "יוגה", "מדיטציה", "צילום", "מוזיקה", "נגינה", "שירה", "איסוף בולים",
+    "איסוף מטבעות", "איסוף כרטיסים", "צפייה בסרטים", "ביקור במוזיאונים", "תיאטרון", "ריקוד"
+  ];
 
   return (
     <div className="form-container">
       <h2 className="title">הוספת דייר</h2>
       <form onSubmit={handleSubmit}>
         <div className="form-row">
-          <input type="text" name="lastName" className="input" placeholder="שם משפחה" value={formData.lastName} onChange={handleChange} />
           <input type="text" name="firstName" className="input" placeholder="שם פרטי" value={formData.firstName} onChange={handleChange} />
+          <input type="text" name="lastName" className="input" placeholder="שם משפחה" value={formData.lastName} onChange={handleChange} />
         </div>
         <div className="form-row">
-          <input type="date" name="birthDate" className="input" placeholder="תאריך לידה" value={formData.birthDate} onChange={handleChange} />
           <input type="text" name="idNumber" className="input" placeholder="תעודת זהות" value={formData.idNumber} onChange={handleChange} />
+          <input type="text" name="birthDate" className="input" placeholder="תאריך לידה" onFocus={(e) => e.target.type = 'date'} onBlur={(e) => e.target.type = 'text'} value={formData.birthDate} onChange={handleChange} />
         </div>
         <div className="form-row">
-          <input type="date" name="entryDate" className="input" placeholder="תאריך כניסה לבית" value={formData.entryDate} onChange={handleChange} />
           <input type="text" name="previousCity" className="input" placeholder="עיר מגורים קודמת" value={formData.previousCity} onChange={handleChange} />
+          <input type="text" name="entryDate" className="input" placeholder="תאריך כניסה לבית" onFocus={(e) => e.target.type = 'date'} onBlur={(e) => e.target.type = 'text'} value={formData.entryDate} onChange={handleChange} />
         </div>
         <div className="form-row">
           <input type="text" name="phone" className="input" placeholder="טלפון" value={formData.phone} onChange={handleChange} />
+          <input type="text" name="additionalPhone" className="input" placeholder="טלפון נוסף" value={formData.additionalPhone} onChange={handleChange} />
+        </div>
+        <div className="form-row">
+          <input type="email" name="email" className="input" placeholder="אימייל" value={formData.email} onChange={handleChange} />
           <input type="text" name="profession" className="input" placeholder="מקצוע" value={formData.profession} onChange={handleChange} />
         </div>
         <div className="form-row">
@@ -77,16 +126,48 @@ const AddTenant = () => {
         </div>
         <div className="form-row">
           <input type="file" name="picture" className="input" placeholder="תמונה" onChange={handleChange} />
-          <input type="text" name="hobbies" className="input" placeholder="חוגים" value={formData.hobbies} onChange={handleChange} />
         </div>
-        <button type="submit" className="button">הוספה</button>
+        <div className="form-row">
+          <select name="hobbies" className="input hobby-select" onChange={handleHobbyChange}>
+            <option value="">בחר תחביב</option>
+            {hobbiesList.map((hobby, index) => (
+              <option key={index} value={hobby}>{hobby}</option>
+            ))}
+          </select>
+        </div>
+        <div className="form-row">
+          <span className="hobby-note">*ניתן להוסיף עד 10 תחביבים שונים</span>
+        </div>
+        <div className="form-row hobbies-container">
+          {formData.selectedHobbies.map((hobby, index) => (
+            <div key={index} className="hobby-item">
+              <input type="checkbox" checked readOnly />
+              <label>{hobby}</label>
+            </div>
+          ))}
+        </div>
+        <button type="submit" className="form-button">הוספה</button>
       </form>
-      {successMessage && (
-        <div className="success-message">
+      {userDetails.username && userDetails.password && (
+        <div className="user-details-modal">
+          <div className="success-message">
           <div>{successMessage}</div>
-          <button onClick={handleSuccessMessageClose} className="button">אישור</button>
+        </div>
+          <div className="user-details">
+            
+            <div className="detail-row">
+              <label>שם משתמש:</label>
+              <input type="text" name="username" className="input" value={userDetails.username} readOnly  />
+            </div>
+            <div className="detail-row">
+              <label>סיסמא:</label>
+              <input type="text" name="password" className="input" value={userDetails.password} readOnly />
+            </div>
+            <button onClick={handleSuccessMessageClose} className="form-button">אישור</button>
+          </div>
         </div>
       )}
+     
     </div>
   );
 };
--- migdalor/src/Components/Home.jsx
+++ migdalor/src/Components/Home.jsx
@@ -1,164 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import styled from 'styled-components';
 import Icons from './Image/Logo1.png';
-import backgroundIMG from './Image/Background.jpg';
 import SendMeassge from './SendMeassge';
 import OpeningHours from './OpeningHours';
 import AddTenant from './AddTenant';
-
-const BackgroundDiv = styled.div`
-  background-image: url(${backgroundIMG});
-  background-size: cover;
-  background-position: center;
-  height: 30vh;
-  width: 100vw;
-`;
-
-const PageContainer = styled.div`
-  font-family: 'Arial', sans-serif;
-  min-height: 100vh;
-  width: 100vw;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: #F6F2E4;
-  color: #fff;
-  margin-left:
-  
-`;
-
-const ContentContainer = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  overflow: auto;
-  width: 100%;
-  padding: 20px;
-  background-color: #38588e;
-  margin-top: 2rem;
-`;
-
-const BurgerButton = styled.div`
-  position: fixed;
-  right: 1rem;
-  top: 0.7rem;
-  padding: 1rem;
-  cursor: pointer;
-  z-index: 2;
-  color: #fff;
-  background-color: #38588e;
-  border-radius: 50%;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-  font-size: 1.5rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 1rem;
-  height: 1rem;
-
-  &:hover {
-    background-color: #2d4f7d;
-  }
-`;
-
-const NavBar = styled.nav`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: #bea029;
-  padding: 0.5rem;
-  width: 100%;
-  margin-bottom: 1rem;
- 
-`;
-
-const StyledIcon = styled.img`
-  width: 150px;
-  height: 150px;
-  margin: 0 auto;
-`;
-
-const ButtonRow = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 10px;
-  margin-top: 1rem;
-  padding: 0 10px;
-`;
-
-const Button = styled.button`
-  background-color: #f0f0f0;
-  padding: 1rem;
-  text-align: center;
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  font-size: 1rem;
-  font-weight: bold;
-  color: #333;
-  cursor: pointer;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #e0e0e0;
-  }
-`;
-
-const SideMenu = styled.div`
-  position: fixed;
-  top: 0;
-  right: ${props => (props.open ? '0' : '-100%')};
-  width: 250px;
-  height: 100%;
-  background-color: #38588e;
-  color: black;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 1rem;
-  transition: right 0.3s ease;
-  box-shadow: -2px 0 5px rgba(0, 0, 0, 0.5);
-  z-index: 1;
-`;
-
-const CloseButton = styled.div`
-  align-self: flex-start;
-  cursor: pointer;
-  font-size: 1.5rem;
-  color: #fff;
-`;
-
-const MenuButton = styled.button`
-  background-color: #ffffff;
-  color: black;
-  padding: 0.5rem;
-  text-align: center;
-  border: none;
-  border-radius: 8px;
-  margin: 0.5rem 0;
-  width: 100%;
-  cursor: pointer;
-  font-size: 0.875rem;
-
-  &:hover {
-    background-color: #e0e0e0;
-  }
-`;
-
-const LogoutButton = styled(MenuButton)`
-  background-color: #ff4d4d;
-  color: white;
-
-  &:hover {
-    background-color: #ff1a1a;
-  }
-`;
-
+import './HomeStyle.css';
 
 const Home = () => {
   const [activeComponent, setActiveComponent] = useState(null);
@@ -180,41 +26,42 @@ const Home = () => {
 
   return (
     <>
-      <BackgroundDiv />
-      <PageContainer>
+      <div className="background-div" />
+      <div className="page-container">
+        {!menuOpen && (
+          <div className="burger-button" onClick={toggleMenu}>☰</div>
+        )}
+        <img className="styled-icon" src={Icons} alt="icon" />
+        <nav className="nav-bar">ברוכים הבאים למערכת ניהול הדיור המוגן</nav>
         
-        <BurgerButton onClick={toggleMenu}>☰</BurgerButton>
-        <StyledIcon src={Icons} alt="icon" />
-        <NavBar>ברוכים הבאים למערכת ניהול הדיור המוגן</NavBar>
-        
-        <ButtonRow>
-          <Button onClick={() => handleClick('form')}>שליחת הודעה</Button>
-          <Button onClick={() => handleClick('hours')}>שעות פתיחה</Button>
-          <Button onClick={() => handleClick('addTenant')}>הוספת דייר</Button>
-          <Button>יצירת פעילות</Button>
-          <Button>כתיבת מודעת אבל</Button>
-          <Button>נוהל בוקר טוב</Button>
-          <Button>ועד דיירים</Button>
-          <Button>עדכון פרטי מחלקה</Button>
-        </ButtonRow>
-        <ContentContainer ref={contentRef}>
+        <div className="button-row">
+          <button className="button" onClick={() => handleClick('form')}>שליחת הודעה</button>
+          <button className="button" onClick={() => handleClick('hours')}>שעות פתיחה</button>
+          <button className="button" onClick={() => handleClick('addTenant')}>הוספת דייר</button>
+          <button className="button">יצירת פעילות</button>
+          <button className="button">כתיבת מודעת אבל</button>
+          <button className="button">נוהל בוקר טוב</button>
+          <button className="button">ועד דיירים</button>
+          <button className="button">עדכון פרטי מחלקה</button>
+        </div>
+        <div className="content-container" ref={contentRef}>
           {activeComponent === 'form' && <SendMeassge />}
           {activeComponent === 'hours' && <OpeningHours />}
           {activeComponent === 'addTenant' && <AddTenant />}
-        </ContentContainer>
-        <SideMenu open={menuOpen}>
-          <CloseButton onClick={toggleMenu}>×</CloseButton>
-          <MenuButton onClick={() => handleClick('form')}>שליחת הודעה</MenuButton>
-          <MenuButton onClick={() => handleClick('hours')}>שעות פתיחה</MenuButton>
-          <MenuButton onClick={() => handleClick('addTenant')}>הוספת דייר</MenuButton>
-          <MenuButton>יצירת פעילות</MenuButton>
-          <MenuButton>כתיבת מודעת אבל</MenuButton>
-          <MenuButton>נוהל בוקר טוב</MenuButton>
-          <MenuButton>ועד דיירים</MenuButton>
-          <MenuButton>עדכון פרטי מחלקה</MenuButton>
-          <LogoutButton>התנתקות</LogoutButton>
-        </SideMenu>
-      </PageContainer>
+        </div>
+        <div className={`side-menu ${menuOpen ? 'open' : ''}`}>
+          <div className="close-button" onClick={toggleMenu}>×</div>
+          <button className="menu-button" onClick={() => handleClick('form')}>שליחת הודעה</button>
+          <button className="menu-button" onClick={() => handleClick('hours')}>שעות פתיחה</button>
+          <button className="menu-button" onClick={() => handleClick('addTenant')}>הוספת דייר</button>
+          <button className="menu-button">יצירת פעילות</button>
+          <button className="menu-button">כתיבת מודעת אבל</button>
+          <button className="menu-button">נוהל בוקר טוב</button>
+          <button className="menu-button">ועד דיירים</button>
+          <button className="menu-button">עדכון פרטי מחלקה</button>
+          <button className="logout-button">התנתקות</button>
+        </div>
+      </div>
     </>
   );
 };
