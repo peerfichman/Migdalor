2c4694ab4bc3ab0c290a077d1a8d70e068a8b983
AnitaSteb <anita10012@gmail.com>
2024-06-02 17:07:06 +0300

    run test for backend

--- Migdalor_Backend/WebApplication1/Controllers/ResidentController.cs
+++ Migdalor_Backend/WebApplication1/Controllers/ResidentController.cs
@@ -12,56 +12,7 @@ namespace WebApplication1.Controllers
         MigdalorContext db = new MigdalorContext();
         Random random = new Random();
 
-        [HttpPost]
-        [Route("AddResident")]
-        //public IActionResult AddResident([FromBody] ResidentDTO userInput)
-        //{
-        //    try
-        //    {
-        //        if (userInput == null)
-        //        {
-        //            return BadRequest("User input is null");
-        //        }
-
-        //        // Check if the username already exists
-        //        if (db.TblResidents.Any(u => u.Username == userInput.Username))
-        //        {
-        //            return BadRequest("Username already exists");
-        //        }
-        //        int randomResidentNumber = random.Next(0, 2147483647);
-
-        //        //
-        //        // לייצר DTO
-        //        var resident = new ResidentDTO
-        //        {
-        //            Username = userInput.Username,
-        //            Password = userInput.Password,
-        //            FirstName = userInput.FirstName,
-        //            LastName = userInput.LastName,
-        //            PhoneNumber = userInput.PhoneNumber,
-        //            Id = userInput.Id,
-        //            DateOfBirth = userInput.DateOfBirth,
-        //            PreviousAddress = userInput.PreviousAddress,
-        //            ResidentImage = userInput.ResidentImage,
-        //            Profession = userInput.Profession,
-        //            Email = userInput.Email
-        //        };
-
-
-
-        //        //Add the user to the context
-        //        db.TblResidents.Add(resident);
-
-        //        //Save changes to the database
-        //        db.SaveChanges();
-
-        //        return Ok("User added successfully");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return StatusCode(500, $"Internal server error: {ex.Message}");
-        //    }
-        //}
+        
         [HttpPost]
         [Route("AddResident")]
         public IActionResult AddResident([FromBody] ResidentDTO userInput)
@@ -100,20 +51,6 @@ namespace WebApplication1.Controllers
                 db.TblResidents.Add(resident);
                 db.SaveChanges();
 
-                //// Add selected hobbies
-                //foreach (var hobbyId in userInput.SelectedHobbies)
-                //{
-                //    var residentHobby = new TblResidentHasHobby
-                //    {
-                //        ResidentNumber = resident.ResidentNumber,
-                //        HobbyNumber = hobbyId
-                //    };
-
-                //    db.TblResidentHasHobbies.Add(residentHobby);
-                //}
-
-                //db.SaveChanges();
-
                 return Ok("User added successfully");
             }
             catch (Exception ex)
@@ -123,28 +60,28 @@ namespace WebApplication1.Controllers
         }
 
 
-        [HttpGet]
-        [Route("GetResidentsWithGoodMorningPolicy")]
-        public IActionResult GetResidentsWithGoodMorningPolicy()
-        {
-            var residents = db.TblResidents
-                .Select(resident => new ResidentWithGoodMorningPolicy
-                {
-                    ResidentNumber = resident.ResidentNumber,
-                    FirstName = resident.FirstName,
-                    LastName = resident.LastName,
-                    PhoneNumber = resident.PhoneNumber,
-                    Id = resident.Id,
-                    DateOfGoodMorningPolicy = db.TblGoodMorningPolicies
-                .Where(gmp => gmp.ResidentNumber == resident.ResidentNumber)
-                .Select(gmp => gmp.DateTime)
-                .FirstOrDefault(),
-                    HasGoodMorningPolicy = db.TblGoodMorningPolicies
-                        .Any(gmp => gmp.ResidentNumber == resident.ResidentNumber)
-                })
-                .ToList();
+        //[HttpGet]
+        //[Route("GetResidentsWithGoodMorningPolicy")]
+        //public IActionResult GetResidentsWithGoodMorningPolicy()
+        //{
+        //    var residents = db.TblResidents
+        //        .Select(resident => new ResidentWithGoodMorningPolicy
+        //        {
+        //            ResidentNumber = resident.ResidentNumber,
+        //            FirstName = resident.FirstName,
+        //            LastName = resident.LastName,
+        //            PhoneNumber = resident.PhoneNumber,
+        //            Id = resident.Id,
+        //            DateOfGoodMorningPolicy = db.TblGoodMorningPolicies
+        //        .Where(gmp => gmp.ResidentNumber == resident.ResidentNumber)
+        //        .Select(gmp => gmp.DateTime)
+        //        .FirstOrDefault(),
+        //            HasGoodMorningPolicy = db.TblGoodMorningPolicies
+        //                .Any(gmp => gmp.ResidentNumber == resident.ResidentNumber)
+        //        })
+        //        .ToList();
 
-            return Ok(residents);
-        }
+        //    return Ok(residents);
+        //}
     }
 }
