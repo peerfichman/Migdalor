a22bb2d3b5c3fb4de593be89aedeeda1856fe4e6
talc22898 <talc228@gmail.com>
2024-07-22 11:18:47 +0300

    new design

--- migdalor/src/Components/OpeningHours.jsx
+++ migdalor/src/Components/OpeningHours.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './OpeningHours.css';
 
 const OpeningHours = () => (
-  <div>
+  <div class="openingHours">
     <h1>שעות פתיחה</h1>
-    <p>ימים ראשון עד חמישי: 9:00 - 17:00</p>
-    <p>יום שישי: 9:00 - 13:00</p>
-    <p>יום שבת: 9:00 - 13:00</p>
+    <p> ראשון - חמישי: 9:00 - 17:00</p>
+    <p> שישי: 9:00 - 13:00</p>
+    <p> שבת: 9:00 - 13:00</p>
     <p>ערב חג: 9:00 - 13:00</p>
   </div>
 );
